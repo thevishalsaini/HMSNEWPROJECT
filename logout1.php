28d0ffc7bc88e0401534865d977563dd41813965
Vishal Saini <98593068+thevishalsaini@users.noreply.github.com>
2024-12-28 10:37:40 +0530

    Update logout1.php

--- logout1.php
+++ logout1.php
@@ -5,6 +5,7 @@ session_destroy();
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
